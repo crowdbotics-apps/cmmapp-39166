bf327e99feabc60e60052e50b74b19f27ad430b1
Crowdbotics <team@crowdbotics.com>
2023-03-01 01:33:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "cmmapp-39166",
+  url: "https://cmmapp-39166.botics.co",
+  api: "https://cmmapp-39166.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
